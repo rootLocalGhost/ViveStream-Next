f4f061821564cd16b4692149d6480735302700dc
root@LocalGhost <rootlocalghost@gmail.com>
2026-08-21 17:09:22 +0600

    feat: disable context menu in production

--- src/index.tsx
+++ src/index.tsx
@@ -2,4 +2,11 @@
 import { render } from "solid-js/web";
 import App from "./App";
 
+if (import.meta.env.PROD) {
+  document.addEventListener("contextmenu", (e) => {
+    e.preventDefault();
+  });
+}
+
 render(() => <App />, document.getElementById("root") as HTMLElement);
+
